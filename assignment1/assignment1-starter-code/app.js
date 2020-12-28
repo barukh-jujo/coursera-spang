02597b85e2178eb25fa3261540e62736f1731030
barukh-jujo <59911482+barukh-jujo@users.noreply.github.com>
2020-12-28 13:20:50 +0800

    assignment 1

---
+++ assignment1/assignment1-starter-code/app.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    angular.module('lunchApp', [])
+        .controller('lunchCtl', lunchCtl);
+
+    lunchCtl.$inject = ['$scope'];
+
+    function lunchCtl($scope) {
+        $scope.result = "";
+        $scope.splitLunch = function(lunchStr) {
+
+            if (lunchStr === undefined || lunchStr === null) {
+                $scope.result = "Please enter data first!";
+            } else {
+                var arr = lunchStr.split(",");
+
+                var noNull = arr.filter(item => item);
+
+                if (noNull.length > 3)
+                    $scope.result = "Too much!";
+                else
+                    $scope.result = "Enjoy!";
+            }
+        };
+
+
+    }
+
+})();
